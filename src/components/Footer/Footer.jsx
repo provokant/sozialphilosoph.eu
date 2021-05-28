b1ce783ae5b5fc6791fd840062d7cc2f4a953188
Michael Czechowski <mail@dailysh.it>
2021-05-28 18:39:12 +0200

    retrieve bg image from graphql query (46m)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
-import { siteDescription, footerLogo, footerLink, footerBackground } from '../../../data/SiteConfig'
+import { siteDescription, footerLogo, footerLink } from '../../../data/SiteConfig'
 import Image from '../Image/Image'
 import './Footer.scss'
 import Question from '../Question/Question'
@@ -15,21 +15,24 @@ export default () => (
             fields: { isActive: { eq: true }, onFooterMenu: { eq: true } }
           }
         ) {
-          edges {
-            node {
-              frontmatter {
-                title
-              }
-              fields {
-                slug
-              }
+          nodes {
+            frontmatter {
+              title
             }
+            fields {
+              slug
+            }
+          }
+        }
+        file(relativePath: {eq: "images/content/sky.jpg"}) {
+          childImageSharp {
+            gatsbyImageData
           }
         }
       }
     `}
-    render={data => (
-      <footer style={{ backgroundImage: `url(${footerBackground})` }}>
+    render={({ allMarkdownRemark, file }) => (
+      <footer style={{ backgroundImage: `url(${file.childImageSharp.gatsbyImageData.images.fallback.src})` }}>
         <Question />
         <nav className="container">
           {(footerLogo && footerLink) && <a href={footerLink} className="logo" rel="noopener noreferrer" target="_blank"><Image src={footerLogo} /></a>}
@@ -37,13 +40,13 @@ export default () => (
           <div className="menu">
             <p className="description">{siteDescription}</p>
             <div className="self-stretch">
-              {data.allMarkdownRemark.edges.map(link => (
+              {allMarkdownRemark.nodes.map(link => (
                 <Link
-                  to={link.node.fields.slug}
-                  key={link.node.fields.slug}
+                  to={link.fields.slug}
+                  key={link.fields.slug}
                   className="text-white py-6 ml-4"
                 >
-                  {link.node.frontmatter.title}
+                  {link.frontmatter.title}
                 </Link>
               ))}
             </div>
